9a8d6981af43cf4896366513b46c18736acc2734
Xiao Huang <yeah.huangx@gmail.com>
2020-04-28 06:08:42 -0400

    4.28 dsu update

--- 128_longestConsecutive.py
+++ 128_longestConsecutive.py
@@ -2,7 +2,7 @@ class Solution:
     def longestConsecutive(self, nums: List[int]) -> int:
 
         #Sol1 我其实都到了强连通图的知识 有点小题大做的意思 anyway 不到40minKO 一次AC还是不错的了～
-        #
+        #76ms
         def findFather(num):
             if father[num]==num:
                 return num
@@ -68,3 +68,57 @@ class Solution:
                 nums.remove(last)
             maxlen = max(maxlen, last - first + 1)
         return maxle
+#sol4 4.28 update 用union find (类似于sol1) 84ms
+class DSU: #Disjoint Set Union 就是常见的染色问题
+    def __init__(self):
+        self.p = {}
+        self.sz = {}
+
+    def find(self, x):
+
+        stack = []
+        while self.p[x] != x:
+            stack.append(x)
+            x = self.p[x]
+        for xx in stack:
+            self.p[xx] = x
+        '''
+        if self.p[x] != x: 会MLE
+            self.p[x] = self.find(self.p[x])
+        '''
+        return x
+
+    def union0(self, x):
+        self.p.setdefault(x,x)
+        self.sz.setdefault(x, 1)   
+        if x - 1 in self.p.keys():
+            self.union(x,x-1)
+        if x + 1 in self.p.keys():
+            self.union(x, x+1)
+            
+    def union(self, x, y):
+
+        px, py = self.find(x), self.find(y)
+        if px == py:
+            return False
+        if self.sz[px] < self.sz[py]:
+            self.sz[py] += self.sz[px]
+            self.p[px] = self.p[py]
+        else:
+            self.sz[px] += self.sz[py]
+            self.p[py] = self.p[px]
+        return True
+    
+
+
+class Solution:
+    def longestConsecutive(self, nums: List[int]) -> int:
+        dsu = DSU()
+        if len(nums)==0:
+            return 0
+        for num in nums:
+            if num not in dsu.p.keys():
+                dsu.union0(num)
+        for num in nums:
+            dsu.find(num)
+        return max(dsu.sz.values())
