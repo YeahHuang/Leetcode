f4e9f2be1059ed2f0f3e277df678401d7e90957c
Xiao Huang <534852491@qq.com>
2020-04-29 16:06:43 -0400

    Add files via upload

--- 496_nextGreaterElementI.py
+++ 496_nextGreaterElementI.py
@@ -1,19 +1,30 @@
 class Solution:
-    def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:#56ms faster than 81.4%
-        nums2.reverse()
-        n = len(nums2)
-        ans = {}
-        stack = []
-        for i in range(n):
-            while stack and nums2[i] >= nums2[stack[-1]]:
-                stack.pop()
-            if stack:
-                ans[nums2[i]] = stack[-1]
-            else:
-                ans[nums2[i]] = -1
-            stack.append(i)
-        return [ans[num] for num in nums1]
-
-    #看一眼大神的O(mn)解法吧
-    def nextGreaterElement(self, findNums, nums):
-        return [next((y for y in nums[nums.index(x):] if y > x), -1) for x in findNums]
+    def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        """
+        
+        10:36 - 
+        
+        最暴力的 对于nums2 哈希 pos[num] = i 
+        然后从前往后走 看看 哪个最大
+        
+        优化： 从后往前 stack
+        当前的比它大 直接pop 
+        
+        从前往后pop
+        """
+        
+        # for i, num in enumerate(nums2):
+        #     pos[num] = i
+        
+        stack = [] #递减的stack
+        m = {}
+        for i, num in enumerate(nums2):
+            #stack为空
+            #>栈顶元素 pop到<
+            #然后append
+            m.setdefault(num, -1)
+            while stack and num > stack[-1]:
+                num0 = stack.pop()
+                m[num0] = num
+            stack.append(num)
+        return [m[num] for num in nums1]
